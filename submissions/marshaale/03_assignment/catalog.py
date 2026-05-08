a0d3c7eb744f6cd37959658226ddb8b4de1c5ece
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-08 14:38:22 +0300

    implement add and list_all catelog & geme methods,attributes

--- submissions/marshaale/03_assignment/catalog.py
+++ submissions/marshaale/03_assignment/catalog.py
@@ -2,10 +2,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Game:
-    pass
+    name:str
+    year:str
+    platform:str # pc, playstation, xbox, nintendo, mobile
+
+    def __str__(self):
+        return f"name: {self.name}, year: {self.year}, platform: {self.platform}"
 
 class Catalog:
-    pass
+    def __init__(self):
+        self.items = []
+    
+    def add(self,catalog):
+        self.items.append(catalog)
+
+    def list_all(self):
+        for item in self.items:
+            print(item)    
 
 
 def load_catalog(path, catalog):
